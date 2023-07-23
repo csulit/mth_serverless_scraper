826a5f952d0dfa93ab4db9a70ad87a314b5bd526
Gelo <chrisgelosulit@gmail.com>
2023-07-23 20:56:17 +0800

    Test pgsql connection

--- packages/hoppler/condominium/index.ts
+++ packages/hoppler/condominium/index.ts
@@ -1,13 +1,17 @@
 import { env } from "../../../utils/env";
+import pgsql from "../../../utils/postgresql";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function main(args: Record<string, any>) {
   try {
+    const foo = pgsql.name;
+
     return {
       statusCode: 200,
       body: {
         env,
         args,
+        foo,
       },
     };
   } catch (error) {
