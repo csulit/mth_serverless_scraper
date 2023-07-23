142085d32d3e8c7c8c3f64d1ec00d13a31e013ae
Gelo <chrisgelosulit@gmail.com>
2023-07-23 19:02:30 +0800

    Test env response on hoppler/condominium

--- packages/hoppler/condominium/index.ts
+++ packages/hoppler/condominium/index.ts
@@ -1,17 +1,12 @@
-import { mongodbClient } from "../../../utils/mongodb";
+import { env } from "../../../utils/env";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function main(args: Record<string, any>) {
   try {
-    const mongodb = await mongodbClient.connect();
-    const db = mongodb.db("mth");
-
-    const test = await db.collection("scraper_api_scrape_data").findOne({});
-
     return {
       statusCode: 200,
       body: {
-        test,
+        env,
         args,
       },
     };
