90b2ff5261fcd22a311f05650112ce82b2ad6cc4
Gelo <chrisgelosulit@gmail.com>
2023-07-23 22:03:16 +0800

    Move database initialization to function itselt

--- packages/hoppler/condominium/index.ts
+++ packages/hoppler/condominium/index.ts
@@ -1,8 +1,17 @@
 import { env } from "../../../utils/env";
-import pgsql from "../../../utils/postgresql";
+import * as postgres from "postgres";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function main(args: Record<string, any>) {
+  const pgsql = postgres({
+    host: env.PG_DATABASE_HOST,
+    port: Number(env.PG_DATABASE_PORT),
+    user: env.PG_DATABASE_USER,
+    password: env.PG_DATABASE_PASS,
+    database: env.PG_DATABASE_NAME,
+    ssl: env.PG_SSL_MODE === "require" ? true : false,
+  });
+
   try {
     const foo = await pgsql`select * from pg_catalog.pg_tables`;
 
