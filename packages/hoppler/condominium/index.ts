869509a8d01d48bb198281836dbad873a6ff1875
Gelo <chrisgelosulit@gmail.com>
2023-07-24 01:18:17 +0800

    Test pg

--- packages/hoppler/condominium/index.ts
+++ packages/hoppler/condominium/index.ts
@@ -1,25 +1,25 @@
-// import { env } from "../../../utils/env";
-// import * as postgres from "postgres";
+import { env } from "../../../utils/env";
+import * as postgres from "postgres";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function main(args: Record<string, any>) {
-  // const pgsql = postgres({
-  //   host: env.PG_DATABASE_HOST,
-  //   port: Number(env.PG_DATABASE_PORT),
-  //   user: env.PG_DATABASE_USER,
-  //   password: env.PG_DATABASE_PASS,
-  //   database: env.PG_DATABASE_NAME,
-  //   ssl: env.PG_SSL_MODE === "require" ? true : false,
-  // });
+  const pgsql = postgres({
+    host: env.PG_DATABASE_HOST,
+    port: Number(env.PG_DATABASE_PORT),
+    user: env.PG_DATABASE_USER,
+    password: env.PG_DATABASE_PASS,
+    database: env.PG_DATABASE_NAME,
+    ssl: env.PG_SSL_MODE === "require" ? true : false,
+  });
 
   try {
-    // const user = await pgsql`select * from user`;
+    const user = await pgsql`select * from user`;
 
     return {
       statusCode: 200,
       body: {
         args,
-        // pg: user.length > 0 ? user[0] : null,
+        pg: user.length > 0 ? user[0] : null,
       },
     };
   } catch (error) {
@@ -29,8 +29,7 @@ export async function main(args: Record<string, any>) {
         error,
       },
     };
+  } finally {
+    await pgsql.end();
   }
-  // finally {
-  //   await pgsql.end();
-  // }
 }
