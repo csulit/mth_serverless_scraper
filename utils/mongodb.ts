adcbe2cf7a8a14ce660c34fee61f572566026742
Gelo <chrisgelosulit@gmail.com>
2023-07-23 19:21:53 +0800

    Remove outdated mongodb client options

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,13 +1,10 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, type MongoClientOptions } from "mongodb";
 import { env } from "./env";
 
 const url = env.MONGO_DATABASE_URL;
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  poolSize: 10, // set the maximum number of connections in the pool
-  serverSelectionTimeoutMS: 5000, // set the timeout for selecting a server
+const options: MongoClientOptions = {
+  maxPoolSize: 10,
   ssl: true,
 };
 
