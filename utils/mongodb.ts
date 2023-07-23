0ece4f76697c2b479dcc71636b4203fa0cedfe5f
Gelo <chrisgelosulit@gmail.com>
2023-07-23 18:52:36 +0800

    Added options in mongodb connection

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -2,4 +2,12 @@ import { MongoClient } from "mongodb";
 import { env } from "./env";
 
 const url = env.MONGO_DATABASE_URL;
-export const mongodbClient = new MongoClient(url);
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 10, // set the maximum number of connections in the pool
+  serverSelectionTimeoutMS: 5000, // set the timeout for selecting a server
+};
+
+export const mongodbClient = new MongoClient(url, options);
