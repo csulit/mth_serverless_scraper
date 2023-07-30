df2af7bd97ce9a97ad5e4ce46315867dbbf5b1c6
Gelo <chrisgelosulit@gmail.com>
2023-07-30 16:55:52 +0800

    Implement response catcher

--- packages/scraper/response-catch/index.ts
+++ packages/scraper/response-catch/index.ts
@@ -1,3 +1,6 @@
+import postgres from "postgres";
+import { env } from "../../../utils/env";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 async function main(args: Record<string, any>) {
   if (args.http.method !== "POST") {
@@ -12,11 +15,32 @@ async function main(args: Record<string, any>) {
     };
   }
 
+  const pgsql = postgres({
+    host: env.PG_DATABASE_HOST,
+    port: Number(env.PG_DATABASE_PORT),
+    user: env.PG_DATABASE_USER,
+    password: env.PG_DATABASE_PASS,
+    database: env.PG_DATABASE_NAME,
+    ssl: env.PG_SSL_MODE === "require" ? "prefer" : false,
+  });
+
   try {
+    const newRow = await pgsql`insert into scraper_api_data (
+        html_data,
+        scraper_api_status,
+        scrape_url
+      ) values (
+        args.response.body,
+        args.status,
+        args.url
+      ) returning html_data_id`;
+
     return {
       statusCode: 200,
       body: {
         success: true,
+        statusCode: 200,
+        newRowId: newRow.length ? newRow[0].html_data_id : null,
       },
     };
   } catch (error) {
