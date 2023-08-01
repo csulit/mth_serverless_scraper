568d961cb41685c4b25f19775cd37b87f368c060
Gelo <chrisgelosulit@gmail.com>
2023-08-01 22:46:03 +0800

    Test get all for lamudi condominium

--- packages/lamudi/condominium/index.ts
+++ packages/lamudi/condominium/index.ts
@@ -1,15 +1,49 @@
-import { collectProperties } from "../../../utils/collectProperties";
+import postgres from "postgres";
+import { env } from "../../../utils/env";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function main(args: Record<string, any>) {
-  const properties = ["foo", "doo"];
+  if (args.http.method !== "POST") {
+    return {
+      statusCode: 405,
+      body: {
+        success: false,
+        statusCode: 405,
+        data: null,
+        message: "Method not allowed",
+      },
+    };
+  }
 
-  const params = collectProperties(args, properties);
+  const pgsql = postgres({
+    host: env.PG_DATABASE_HOST,
+    port: Number(env.PG_DATABASE_PORT),
+    database: env.PG_DATABASE_NAME,
+    user: env.PG_DATABASE_USER,
+    password: env.PG_DATABASE_PASS,
+    ssl: env.PG_SSL_MODE === "require" ? "prefer" : false,
+  });
 
-  return {
-    statusCode: 200,
-    body: {
-      params,
-    },
-  };
+  try {
+    const condominium =
+      await pgsql`select * from scraper_api_data where scrape_url 
+      like '%https://www.lamudi.com.ph/condominium%' limit 10`;
+
+    return {
+      statusCode: 200,
+      body: {
+        success: true,
+        condominium,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: error?.status ?? 500,
+      body: {
+        error: error?.message ?? "Internal Server Error",
+      },
+    };
+  }
 }
